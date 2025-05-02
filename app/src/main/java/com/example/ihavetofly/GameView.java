f05885d1cba89d7d31d06bfdfe9b5b76a4e3bd70
MayankYadav0021 <mayank11183yadav@gmail.com>
2025-05-02 15:42:08 +0530

    Updated project with latest code

--- app/src/main/java/com/example/ihavetofly/GameView.java
+++ app/src/main/java/com/example/ihavetofly/GameView.java
@@ -3,6 +3,7 @@ package com.example.ihavetofly;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements Runnable{
@@ -12,6 +13,7 @@ public class GameView extends SurfaceView implements Runnable{
     private int screenX,screenY;
     public static float screenRatioX,screenRatioY;
     private Paint paint;
+    private Flight flight;
     private Background background1,background2;
     public GameView(Context context, int screenX, int screenY) {
         super(context);
@@ -23,6 +25,9 @@ public class GameView extends SurfaceView implements Runnable{
 
         background1 = new Background(screenX,screenY, getResources());
         background2 = new Background(screenX,screenY, getResources());
+
+        flight = new Flight(screenY, getResources());
+
         background2.x = screenX;
 
         paint = new Paint();
@@ -47,12 +52,22 @@ public class GameView extends SurfaceView implements Runnable{
         if(background2.x + background2.background.getWidth()<0){
             background2.x = screenX;
         }
+        if(flight.isGoingUp)
+            flight.y -= 30*screenRatioY;
+        else
+            flight.y += 30*screenRatioY;
+        if(flight.y < 0)
+            flight.y = 0;
+        if(flight.y > screenY - flight.height)
+            flight.y = screenY - flight.height;
     }
     private void draw(){
         if (getHolder().getSurface().isValid()){
             Canvas canvas = getHolder().lockCanvas();
             canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
             canvas.drawBitmap(background2.background, background1.x, background2.y, paint);
+
+            canvas.drawBitmap(flight.getFlight(),flight.x,flight.y,paint);
 
             getHolder().unlockCanvasAndPost(canvas);
         }
@@ -79,5 +94,18 @@ public class GameView extends SurfaceView implements Runnable{
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                if(event.getX()< screenX/2){
+                    flight.isGoingUp = true;
+                }
+            case MotionEvent.ACTION_UP:
+                flight.isGoingUp = false;
+        }
+        return true;
     }
 }
